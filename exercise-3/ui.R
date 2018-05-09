9c4621e24076eebfc68192f2a91278850af1e65d
Leo <Leo3418@users.noreply.github.com>
2018-05-09 16:29:16 -0700

    Complete exercise 3

--- exercise-3/ui.R
+++ exercise-3/ui.R
@@ -1,25 +1,27 @@
 # UI for scatterplot
 library(shiny)
+library(ggplot2)
 
 # Get a vector of column names (from `mpg`) to use as select inputs
 select_values <- colnames(mpg)
 
 # Create a shinyUI with a `fluidPage` layout
-
+shinyUI(fluidPage(
   # A page header
-
+  h1("MPG Dataset Exploration"),
   
   # Add a select input for the x variable
-
+  selectInput("x_var", "X variable", select_values, selected = "displ"),
   
   # Add a select input for the y variable
-
+  selectInput("y_var", "Y variable", select_values, selected = "cyl"),
 
   # Add a sliderInput to set the size of each point
-
+  sliderInput("size", "Size of point", 1, 10, 7),
 
   # Add a selectInput that allows you to select a color from a list of choices
-
-
+  selectInput("color", "Color", c("red", "blue", "green"), selected = "blue"),
 
   # Plot the output with the name "scatter"
+  plotOutput("scatter")
+))
